97d6cca5bd0d5268a679ecda4dad5fc4b421e2db
Kuno Woudt <kuno@frob.nl>
2022-12-04 10:46:36 -0500

    Update dep

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ fn main() -> ! {
     loop {
         info!("on!");
         led_pin.set_high().unwrap();
-        delay.delay_ms(500);
+        delay.delay_ms(1000);
         info!("off!");
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
